8736775dfc334d4fc3a5a8d61668cb964da71d2c
Jerome Tan <DevJerome@iCloud.com>
2016-03-28 23:54:20 +0800

    Add constraints

--- LocationPicker/LocationPicker.swift
+++ LocationPicker/LocationPicker.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 public class LocationPicker: UIViewController {
+    
+    private let searchBar = UISearchBar()
+    private let tableView = UITableView()
 
     public override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = UIColor.whiteColor()
         
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+        
+        layoutViews()
+    }
+    
+    private func layoutViews() {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let margins = view.layoutMarginsGuide
+        
+        searchBar.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor).active = true
+        searchBar.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor, constant: -view.layoutMargins.left * 2).active = true
+        searchBar.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor, constant: view.layoutMargins.right * 2).active = true
+        
+        tableView.topAnchor.constraintEqualToAnchor(searchBar.bottomAnchor).active = true
+        tableView.leadingAnchor.constraintEqualToAnchor(searchBar.leadingAnchor).active = true
+        tableView.trailingAnchor.constraintEqualToAnchor(searchBar.trailingAnchor).active = true
+        tableView.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor).active = true
     }
 
 }
