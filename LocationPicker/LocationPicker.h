ebb9db6cf9990c266cf5b150033a78a0035d6841
Jerome Tan <DevJerome@iCloud.com>
2016-03-28 18:06:08 +0800

    Initial Commit

---
+++ LocationPicker/LocationPicker.h
@@ -0,0 +1,19 @@
+//
+//  LocationPicker.h
+//  LocationPicker
+//
+//  Created by Jerome Tan on 3/28/16.
+//  Copyright © 2016 Jerome Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocationPicker.
+FOUNDATION_EXPORT double LocationPickerVersionNumber;
+
+//! Project version string for LocationPicker.
+FOUNDATION_EXPORT const unsigned char LocationPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationPicker/PublicHeader.h>
+
+
