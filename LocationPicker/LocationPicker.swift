de7181273a8d6d584ddd4a99a3ce3a635b6adb96
Jerome Tan <DevJerome@iCloud.com>
2016-03-28 18:14:09 +0800

    Add example app

---
+++ LocationPicker/LocationPicker.swift
@@ -0,0 +1,19 @@
+//
+//  LocationPicker.swift
+//  LocationPicker
+//
+//  Created by Jerome Tan on 3/28/16.
+//  Copyright © 2016 Jerome Tan. All rights reserved.
+//
+
+import UIKit
+
+public class LocationPicker: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
