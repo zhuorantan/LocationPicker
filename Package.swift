37f3c546d896341dd79ec37cc0e219c2e2b97a53
Pedro José Pereira Vieito <pvieito@gmail.com>
2020-04-02 14:21:07 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,22 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
-let package = Package(name: "LocationPicker")
+let package = Package(
+    name: "LocationPicker",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "LocationPicker",
+            targets: ["LocationPicker"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "LocationPicker",
+            path: "LocationPicker"
+        )
+    ]
+)
